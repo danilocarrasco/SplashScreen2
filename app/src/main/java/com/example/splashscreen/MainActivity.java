b22edbe4ae07bb2fec523fb9edf47605abee3349
daniloabrao <danilo.eletronica@gmail.com>
2019-02-25 19:17:23 -0300

    splash funcionando

--- app/src/main/java/com/example/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.splashscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt_sair = findViewById(R.id.bt_sair);
+        bt_sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
+
 }
